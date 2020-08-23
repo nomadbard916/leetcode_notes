cf5d9992d015079e0b0507e1ba2570ede046bbfd
nomadbard916 <neonnova916@gmail.com>
2020-08-23 17:36:24 +0800

    12

---
+++ 12.integer-to-roman.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode id=12 lang=python3
+#
+# [12] Integer to Roman
+#
+
+# @lc code=start
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        symbol_val: dict = {
+            "M": 1000,
+            "CM": 900,
+            "D": 500,
+            "CD": 400,
+            "C": 100,
+            "XC": 90,
+            "L": 50,
+            "XL": 40,
+            "X": 10,
+            "IX": 9,
+            "V": 5,
+            "IV": 4,
+            "I": 1,
+        }
+        ans = ""
+
+        for k, v in symbol_val.items():
+            while num >= v:
+                ans += k
+                num -= v
+
+        return ans
+
+
+# @lc code=end
+
