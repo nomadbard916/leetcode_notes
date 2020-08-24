72709d8d91d70cc979057094047050dc22e80653
nomadbard916 <neonnova916@gmail.com>
2020-08-24 20:22:16 +0800

    69

---
+++ 69.sqrt-x.py
@@ -0,0 +1,17 @@
+from math import sqrt
+
+#
+# @lc app=leetcode id=69 lang=python3
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+class Solution:
+    def mySqrt(self, x: int) -> int:
+
+        return int(sqrt(x))
+
+
+# @lc code=end
+
