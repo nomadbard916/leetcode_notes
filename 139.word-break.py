64aa68f40a07b23f6def037ee658e9bb671ced97
nomadbard916 <neonnova916@gmail.com>
2020-08-24 20:03:50 +0800

    139 with notes

--- 139.word-break.py
+++ 139.word-break.py
@@ -12,7 +12,7 @@ class Solution:
         # return true when the whole string is done comparison
         s_length: int = len(s)
 
-        DPtable: dict = {}
+        memo: dict = {}
 
         # sanity check
         if s_length == 0 or len(wordDict) == 0:
@@ -23,23 +23,24 @@ class Solution:
 
             # ending condition: the ramaining string exist in wordDict
             if option_list in wordDict:
-                DPtable[option_list] = True
+                memo[option_list] = True
                 return True
 
             # ending condition: the ramaining string is already recorded to be breakable
-            if option_list in DPtable:
-                return DPtable[option_list]
+            if option_list in memo:
+                return memo[option_list]
 
             for i in range(0, option_length):
-                # checking from the end, the recursively checking the previous parts
-                checking = option_list[i:]
-                checkable = option_list[:i]
+                # checking from the beginning, the recursively checking the remaining parts
+                checking = option_list[:i]
+                checkable = option_list[i:]
 
+                # if a part is breakable, then the remaining part must also be breakable
                 if checking in wordDict and backtrack(checkable):
-                    DPtable[option_list] = True
+                    memo[option_list] = True
                     return True
 
-            DPtable[option_list] = False
+            memo[option_list] = False
             return False
 
         return backtrack()
