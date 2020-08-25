25fd31a4f21e7597e58ce53e701722b76ab274ef
nomadbard916 <neonnova916@gmail.com>
2020-08-25 22:48:26 +0800

    326

---
+++ 326.power-of-three.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=326 lang=python3
+#
+# [326] Power of Three
+#
+
+# @lc code=start
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 3 == 0:
+            n = int(n / 3)
+        return n == 1
+
+
+# @lc code=end
+
