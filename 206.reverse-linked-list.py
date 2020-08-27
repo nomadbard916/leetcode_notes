09b08b1915b9447d24ec3220212d0e711409a914
nomadbard916 <neonnova916@gmail.com>
2020-08-27 23:05:19 +0800

    206

--- 206.reverse-linked-list.py
+++ 206.reverse-linked-list.py
@@ -12,6 +12,19 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
-        
+        prev = None
+
+        while head:
+            # find next node when current(head) and previous nodes are known
+            next_node = head.next
+
+            head.next = prev
+
+            prev = head
+            head = next_node
+
+        return prev
+
+
 # @lc code=end
 
