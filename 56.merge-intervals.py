a87d665d39a3cce9b83b85661ff3875579f98f06
nomadbard916 <neonnova916@gmail.com>
2020-08-27 15:48:17 +0800

    56

--- 56.merge-intervals.py
+++ 56.merge-intervals.py
@@ -7,6 +7,31 @@
 # @lc code=start
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
+        # append the first period, then check if the following ones can be combined with the previous one
+        # combine or append new, then repeatedly check every one until the last
+
+        # sanity check
+        if not intervals:
+            return []
+
+        intervals.sort(key=lambda i: i[0])
+
+        ans = []
+        ans.append(intervals[0])
+
+        for i, current_interval in enumerate(intervals):
+            if i == 0:  # already processed initially
+                continue
+
+            prev_interval = ans[-1]
+
+            if current_interval[0] <= prev_interval[1]:
+                prev_interval[1] = max(prev_interval[1], current_interval[1])
+            else:
+                ans.append(current_interval)
+
+        return ans
+
+
 # @lc code=end
 
