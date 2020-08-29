22f8908126e6de5ce8cdd3a675ae0beb76056259
nomadbard916 <neonnova916@gmail.com>
2020-08-29 17:52:44 +0800

    33 with notes

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -7,6 +7,35 @@
 # @lc code=start
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        
+        # binary search, consider current asc or desc ordering
+        # sanity check
+        if not nums:
+            return -1
+
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            # in asc order
+            if nums[mid] < nums[right]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            # in desc order
+            else:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+        return -1
+
+        # sol 2: with built-in
+        # return nums.index(target)
+
+
 # @lc code=end
 
