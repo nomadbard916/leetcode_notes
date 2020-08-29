c987639068b3e08e61243afbae90bac44709ddab
nomadbard916 <neonnova916@gmail.com>
2020-08-29 18:05:26 +0800

    234

--- 234.palindrome-linked-list.py
+++ 234.palindrome-linked-list.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 #
 # @lc app=leetcode id=234 lang=python3
 #
@@ -12,6 +14,24 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
-        
+        if not head or not head.next:
+            return True
+
+        tmp_list = []
+        while head:
+            tmp_list.append(head.val)
+            head = head.next
+
+        l, r = 0, len(tmp_list) - 1
+
+        while l < r:
+            if tmp_list[l] != tmp_list[r]:
+                return False
+            l += 1
+            r -= 1
+
+        return True
+
+
 # @lc code=end
 
