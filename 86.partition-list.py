9b6f1b4b416920726b0d15cc8026149c63f84775
nomadbard916 <neonnova916@gmail.com>
2020-08-29 19:06:22 +0800

    86

--- 86.partition-list.py
+++ 86.partition-list.py
@@ -12,6 +12,24 @@
 #         self.next = next
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
-        
+        before_node = dummy_before = ListNode()
+        after_node = dummy_after = ListNode()
+
+        while head:
+            if head.val < x:
+                before_node.next = head
+                before_node = before_node.next
+            else:
+                after_node.next = head
+                after_node = after_node.next
+
+            head = head.next
+
+        after_node.next = None
+        before_node.next = dummy_after.next
+
+        return dummy_before.next
+
+
 # @lc code=end
 
