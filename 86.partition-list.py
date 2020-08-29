6fa413252b62abab7a7d88b1e86ef5275f8a7f80
nomadbard916 <neonnova916@gmail.com>
2020-08-29 19:10:33 +0800

    86 with notes

--- 86.partition-list.py
+++ 86.partition-list.py
@@ -12,9 +12,12 @@
 #         self.next = next
 class Solution:
     def partition(self, head: ListNode, x: int) -> ListNode:
+        # use dummy nodes as outsiders and start from dummy
         before_node = dummy_before = ListNode()
         after_node = dummy_after = ListNode()
 
+        # traverse the linked list
+        # find the heads of two lists and link them in the process
         while head:
             if head.val < x:
                 before_node.next = head
@@ -25,6 +28,9 @@ class Solution:
 
             head = head.next
 
+        # when the traversal is done, so are the linking of two lists
+        # but still need to cut the link for after list
+        # and link before and after lists together
         after_node.next = None
         before_node.next = dummy_after.next
 
