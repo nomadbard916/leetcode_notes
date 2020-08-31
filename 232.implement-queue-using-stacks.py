c61208fa95ce3a85eb19afcbf89898b6cd2b1a9d
nomadbard916 <neonnova916@gmail.com>
2020-08-31 19:42:09 +0800

    232

---
+++ 232.implement-queue-using-stacks.py
@@ -0,0 +1,62 @@
+from collections import deque
+
+#
+# @lc app=leetcode id=232 lang=python3
+#
+# [232] Implement Queue using Stacks
+#
+
+# @lc code=start
+class MyQueue:
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.inStack, self.outStack = [], []
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+
+        self.inStack.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+
+        temp = self.peek()
+        self.outStack.pop()
+
+        return temp
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        # inverse the sequence of stack items
+        # compared to # 255's push(), it's very hard to inverse sequence using only one stack,
+        # therefore a second stack for taking out is needed
+        if not self.outStack:
+            while self.inStack:
+                self.outStack.append(self.inStack.pop())
+
+        return self.outStack[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+
+        return len(self.inStack) == 0 and len(self.outStack) == 0
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
+# @lc code=end
+
