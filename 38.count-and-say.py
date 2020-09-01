ee4266edff121e05c5dbef0a202ad110ba425ea2
nomadbard916 <neonnova916@gmail.com>
2020-09-01 18:57:26 +0800

    solution 2 for 38 with itertools.groupby

--- 38.count-and-say.py
+++ 38.count-and-say.py
@@ -29,6 +29,12 @@ class Solution:
 
         return s
 
+        # sol2
+        # res = '1'
+        # for i in xrange(n-1):
+        #     res = ''.join([str(len(list(group))) + digit for digit, group in itertools.groupby(res)])
+        # return res
+
 
 # @lc code=end
 
