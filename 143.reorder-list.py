da85a3fe5771644043b51bc5cd39368133bbe815
nomadbard916 <neonnova916@gmail.com>
2020-09-02 17:25:43 +0800

    143

--- 143.reorder-list.py
+++ 143.reorder-list.py
@@ -15,6 +15,82 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        
+
+        # previous half goes forward, latter half backward,
+        # then concat two iteratively
+
+        # sanity check:  length of list should be >= 3
+        if head is None or head.next is None or head.next.next is None:
+            return head
+
+        # find mid and cut list from half
+        fast = slow = head
+
+        while fast and fast.next:
+            fast, slow = fast.next.next, slow.next
+
+        head1, head2 = head, slow.next
+        slow.next = None
+
+        # reverse latter half list with head2
+        dummy = ListNode()
+        dummy.next = head2
+
+        p = head2.next
+        head2.next = None
+
+        while p:
+            temp = p
+
+            p = p.next
+            temp.next = dummy.next
+            dummy.next = temp
+
+        head2 = dummy.next
+
+        #  merge two lined list head1 and head2
+        p1, p2 = head1, head2
+
+        while p2:
+            temp1, temp2 = p1.next, p2.next
+
+            p1.next, p2.next = p2, temp1
+            p1, p2 = temp1, temp2
+
+        # not a feasible solution
+        # but it's good example why list may not be a good container for this solution:
+        # original head cannot be found
+
+        # container = []
+        # current_node = head
+        # while current_node:
+        #     container.append(current_node)
+        #     current_node = current_node.next
+
+        # length = len(container)
+        # if length <= 2:
+        #     return head
+
+        # mid_index = length // 2
+
+        # first_half = container[:mid_index]
+        # second_half = container[mid_index::-1]
+
+        # prev = dummy = ListNode()
+        # dummy.next = head
+
+        # while first_half:
+        #     f_node = first_half.pop(0)
+
+        #     s_node = second_half.pop(0) if second_half else None
+
+        #     prev.next = f_node
+        #     f_node.next = s_node
+
+        #     prev = s_node
+
+        # return dummy.next
+
+
 # @lc code=end
 
