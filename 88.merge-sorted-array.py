88e8c9a0dcafda49874740bd91c52bba0e58a09f
nomadbard916 <neonnova916@gmail.com>
2020-09-02 11:59:51 +0800

    88

---
+++ 88.merge-sorted-array.py
@@ -0,0 +1,34 @@
+#
+# @lc app=leetcode id=88 lang=python3
+#
+# [88] Merge Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i, j = m - 1, n - 1  # start at last position with meaningful data
+        k = m + n - 1  # insertion position
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            if nums1[i] < nums2[j]:
+                nums1[k] = nums2[j]
+                j -= 1
+
+            k -= 1
+
+        # when every num in nums2 is bigger then nums1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
+
+
+# @lc code=end
+
