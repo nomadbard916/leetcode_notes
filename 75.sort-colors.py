ce6bcdd151812b824618bb5751338bbb4b561300
nomadbard916 <neonnova916@gmail.com>
2020-09-03 18:05:24 +0800

    73

---
+++ 75.sort-colors.py
@@ -0,0 +1,30 @@
+from collections import Counter
+
+#
+# @lc app=leetcode id=75 lang=python3
+#
+# [75] Sort Colors
+#
+
+# @lc code=start
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # counting sort
+        # or use any other sorting methods, even bubble sort
+
+        count = Counter(nums)
+
+        for i, num in enumerate(nums):
+            if i < count[0]:
+                nums[i] = 0
+            elif i < count[0] + count[1]:
+                nums[i] = 1
+            else:
+                nums[i] = 2
+
+
+# @lc code=end
+
