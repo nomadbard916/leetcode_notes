89afd3b524aaec31d54572d8223684bff79dc703
nomadbard916 <neonnova916@gmail.com>
2020-09-03 13:03:39 +0800

    46

---
+++ 46.permutations.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode id=46 lang=python3
+#
+# [46] Permutations
+#
+
+# @lc code=start
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perm_n = len(nums)
+
+        ans = []
+
+        def backtrack(current_path=[]):
+            if len(current_path) == perm_n:
+                ans.append(current_path)
+                return
+
+            for num in nums:
+                if num in current_path:
+                    continue
+                else:
+                    updated_path = current_path + [num]
+
+                backtrack(updated_path)
+
+        backtrack()
+
+        return ans
+
+
+# @lc code=end
+
