a2c5cdd7bc4ff0c7f09539074fe2c4aeb2209a93
nomadbard916 <neonnova916@gmail.com>
2021-09-21 19:49:43 +0800

    162

---
+++ 162.find-peak-element.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=162 lang=python3
+#
+# [162] Find Peak Element
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        # O(log n) => binary search
+        # need to check if it's going up or down, therefore mid and its helper
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            mid_helper = mid + 1
+
+            if nums[mid] < nums[mid_helper]:
+                l = mid_helper
+            else:
+                r = mid
+
+        return l
+
+
+# @lc code=end
+
