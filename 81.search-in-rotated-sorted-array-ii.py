1fc71768d5b54fb2e129f376aa693a8ca379bfa3
nomadbard916 <neonnova916@gmail.com>
2021-09-21 19:28:47 +0800

    81 simple solution

--- 81.search-in-rotated-sorted-array-ii.py
+++ 81.search-in-rotated-sorted-array-ii.py
@@ -38,6 +38,18 @@ class Solution:
                     r = mid - 1
         return False
 
+        # sol2: simply do binary search
+        # l, r = 0, len(nums) - 1
+        # while l < r:
+        #     m = (l + r) // 2
+        #     if nums[m] < nums[r]:
+        #         r = m
+        #     elif nums[m] > nums[r]:
+        #         l = m + 1
+        #     else:
+        #         r -= 1
+        # return nums[l]
+
 
 # @lc code=end
 
