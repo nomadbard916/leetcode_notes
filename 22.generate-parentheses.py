dab6d76eb8a6a3c17237efd59c429eeea3789180
nomadbard916 <neonnova916@gmail.com>
2021-09-26 17:11:56 +0800

    22

--- 22.generate-parentheses.py
+++ 22.generate-parentheses.py
@@ -15,14 +15,16 @@ class Solution:
         self.dfs(res, n, n, "")
         return res
 
-    def dfs(self, res, left, right, path):
-        if left == 0 and right == 0:
+    def dfs(self, res, left_addable, right_addable, path):
+        # right can be added when there's more left than right
+        if left_addable == 0 and right_addable == 0:
             res.append(path)
             return
-        if left > 0:
-            self.dfs(res, left - 1, right, path + "(")
-        if left < right:
-            self.dfs(res, left, right - 1, path + ")")
+        if left_addable > 0:
+            self.dfs(res, left_addable - 1, right_addable, path + "(")
+        # add ) when there's more ( than )
+        if left_addable < right_addable:
+            self.dfs(res, left_addable, right_addable - 1, path + ")")
 
 
 # @lc code=end
