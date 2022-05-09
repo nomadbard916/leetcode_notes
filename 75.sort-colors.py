d347ebae60a0fa7fd86fdacef71b0565df4fa255
nomadbard916 <neonnova916@gmail.com>
2022-05-09 20:00:31 +0800

    75 sol2: three-direction quick sort

--- 75.sort-colors.py
+++ 75.sort-colors.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from typing import List
 
 #
 # @lc app=leetcode id=75 lang=python3
@@ -7,24 +8,40 @@ from collections import Counter
 #
 
 # @lc code=start
+
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # counting sort
-        # or use any other sorting methods, even bubble sort
+        # sol1: counting sort, space complexity O(N)
 
-        count = Counter(nums)
+        counter = Counter(nums)
 
-        for i, num in enumerate(nums):
-            if i < count[0]:
+        for i, _ in enumerate(nums):
+            if i < counter[0]:
                 nums[i] = 0
-            elif i < count[0] + count[1]:
+            elif i < counter[0] + counter[1]:
                 nums[i] = 1
             else:
                 nums[i] = 2
 
+        #  sol2: two pointers, one pass with O(nlog(n)) (T) / O(1) (S)
+        # p0 as the righter most of 0, p2 as lefter most of 2
+        # use i to start iteration, with initial p0, p2 = (0, len(nums)-1)
+        # p0, i, p2 = 0, 0, len(nums) - 1
+        # while i <= p2:
+        #     if nums[i] == 0:
+        #         # in-place swap
+        #         nums[p0], nums[i] = nums[i], nums[p0]
+        #         p0 += 1
+        #         i += 1
+        #     elif nums[i] == 2:
+        #         nums[i], nums[p2] = nums[p2], nums[i]
+        #         p2 -= 1
+        #     else:
+        #         i += 1
+
 
 # @lc code=end
-
