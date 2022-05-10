7a7e94fa9491d6846dbfc15fa5b85af73c9c6757
nomadbard916 <neonnova916@gmail.com>
2022-05-10 22:32:03 +0800

    5

---
+++ 5.longest-palindromic-substring.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=5 lang=python3
+#
+# [5] Longest Palindromic Substring
+#
+
+# @lc code=start
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest = ""
+
+        def findLongest(s, l, r):
+            while l >= 0 and r < len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
+            return s[l + 1 : r]
+
+        for i in range(len(s)):
+            s1 = findLongest(s, i, i)
+            if len(s1) > len(longest):
+                longest = s1
+
+            s2 = findLongest(s, i, i + 1)
+            if len(s2) > len(longest):
+                longest = s2
+
+        return longest
+
+
+# @lc code=end
