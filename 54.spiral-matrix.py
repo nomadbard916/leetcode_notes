455bbad479c7488f4ee20537402e43b0f2fd02b9
nomadbard916 <neonnova916@gmail.com>
2022-05-14 15:33:11 +0800

    clarify 54

--- 54.spiral-matrix.py
+++ 54.spiral-matrix.py
@@ -5,6 +5,9 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         # https://blog.csdn.net/fuxuemingzhu/article/details/79541501
@@ -13,42 +16,40 @@ class Solution:
         if not matrix or not matrix[0]:
             return ans
 
-        M, N = len(matrix), len(matrix[0])
+        H, W = len(matrix), len(matrix[0])
 
         # position index of borders
-        left, right, top, bottom = 0, N - 1, 0, M - 1
+        l_border, r_border, top_border, btm_border = 0, W - 1, 0, H - 1
 
         # set initial coordination
         (x, y) = (0, 0)
 
         # set coordination changes by  → ↓ ← ↑
-        movements = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-        # 0: →, 1: ↓, 2: ← 3: ↑
-        direction = 0
+        MOVEMENTS = {"right": (1, 0), "down": (0, 1), "left": (-1, 0), "up": (0, -1)}
 
-        while len(ans) != M * N:
+        direction = "right"
+
+        while len(ans) != H * W:
             ans.append(matrix[y][x])
 
             # going right until reaching right border
-            if direction == 0 and x == right:
-                direction += 1
-                top += 1
+            if direction == "right" and x == r_border:
+                direction = "down"
+                top_border += 1
             # going down until reaching bottom border
-            elif direction == 1 and y == bottom:
-                direction += 1
-                right -= 1
+            elif direction == "down" and y == btm_border:
+                direction = "left"
+                r_border -= 1
             # going left until reaching left border
-            elif direction == 2 and x == left:
-                direction += 1
-                bottom -= 1
+            elif direction == "left" and x == l_border:
+                direction = "up"
+                btm_border -= 1
             # going up until reaching top border
-            elif direction == 3 and y == top:
-                direction += 1
-                left += 1
+            elif direction == "up" and y == top_border:
+                direction = "right"
+                l_border += 1
 
-            direction %= 4
-
-            x, y = tuple(map(sum, zip((x, y), movements[direction])))
+            x, y = (x + MOVEMENTS[direction][0], y + MOVEMENTS[direction][1])
 
         return ans
 
