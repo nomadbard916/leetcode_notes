9d72efb50af78d0f74eb31eaccf374e14c12446e
nomadbard916 <neonnova916@gmail.com>
2022-05-14 20:55:04 +0800

    704

---
+++ 704.binary-search.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            mid_num: int = nums[mid]
+            if mid_num == target:
+                return mid
+            if mid_num < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return -1
+
+
+# @lc code=end
