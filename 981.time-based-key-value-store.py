337a1d1210ef3314b048f2fd10d8334283fcf3e0
nomadbard916 <neonnova916@gmail.com>
2022-05-15 17:01:45 +0800

    981

--- 981.time-based-key-value-store.py
+++ 981.time-based-key-value-store.py
@@ -5,16 +5,38 @@
 #
 
 # @lc code=start
-class TimeMap:
+from bisect import bisect
+from collections import defaultdict
 
+
+class TimeMap:
     def __init__(self):
-        
+        # need to get value by key => dict
+        # while search by value, timestamp pair
+        # => two lists to store value and timestamp for the same key in dict
+        self._t = defaultdict(list)
+        self._v = defaultdict(list)
+        # * self._max_t helps to return early when timestamp already exceeds the max,
+        # but not necessary
+        # self._max_t = defaultdict(int)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        
+        self._t[key].append(timestamp)
+        self._v[key].append(value)
+        # self._max_t[key] = max(self._max_t[key], timestamp)
 
     def get(self, key: str, timestamp: int) -> str:
-        
+        if key not in self._t:
+            return ""
+
+        # if timestamp >= self._max_t[key]:
+        #     return self._v[key][-1]
+
+        if timestamp := bisect(self._t[key], timestamp):
+            real_index = timestamp - 1
+            return self._v[key][real_index]
+
+        return ""
 
 
 # Your TimeMap object will be instantiated and called as such:
@@ -22,4 +44,3 @@ class TimeMap:
 # obj.set(key,value,timestamp)
 # param_2 = obj.get(key,timestamp)
 # @lc code=end
-
