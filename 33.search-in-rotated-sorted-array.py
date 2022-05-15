a475138d23dc35eb1c16ce5dd9848bd7c58ad54e
nomadbard916 <neonnova916@gmail.com>
2022-05-15 14:56:06 +0800

    refactor 33

--- 33.search-in-rotated-sorted-array.py
+++ 33.search-in-rotated-sorted-array.py
@@ -5,6 +5,9 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         # binary search, consider current asc or desc ordering
@@ -13,29 +16,38 @@ class Solution:
             return -1
 
         left, right = 0, len(nums) - 1
-
         while left <= right:
             mid = (left + right) // 2
-
             if nums[mid] == target:
                 return mid
-            # in asc order
-            if nums[mid] < nums[right]:
-                if target > nums[mid] and target <= nums[right]:
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            # in desc order
-            else:
-                if target < nums[mid] and target >= nums[left]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
+
+            m_to_r_ordered = nums[mid] < nums[right]
+            if m_to_r_ordered:
+                left, right = self.assign_m_to_r(nums, target, left, right, mid)
+            else:  # l_to_m_ordered
+                left, right = self.assign_l_to_m(nums, target, left, right, mid)
         return -1
 
-        # sol 2: with built-in
+        # sol 2: with built-in as range nums is relatively small
         # return nums.index(target)
+
+    def assign_l_to_m(
+        self, nums, target, left: int, right: int, mid
+    ) -> tuple[int, int]:
+        if target < nums[mid] and target >= nums[left]:
+            right = mid - 1
+        else:
+            left = mid + 1
+        return left, right
+
+    def assign_m_to_r(
+        self, nums, target, left: int, right: int, mid
+    ) -> tuple[int, int]:
+        if target > nums[mid] and target <= nums[right]:
+            left = mid + 1
+        else:
+            right = mid - 1
+        return left, right
 
 
 # @lc code=end
-
