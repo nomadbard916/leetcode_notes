78a712249e15c983e38a02b9259a16b54f29de77
nomadbard916 <neonnova916@gmail.com>
2022-05-18 13:20:15 +0800

    clarify 98

--- 98.validate-binary-search-tree.py
+++ 98.validate-binary-search-tree.py
@@ -6,23 +6,28 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        def DFS(root, min=float("-INF"), max=float("INF")):
+        def DFS(root, min, max):
+            # traverse all the way into void and it's sure to be valid
             if not root:
                 return True
 
+            # a BST's children values must be within limited upper & lower bounds
             if root.val >= max or root.val <= min:
                 return False
+
             return DFS(root.left, min, root.val) and DFS(root.right, root.val, max)
 
-        return DFS(root)
+        # at first the real min/max are unknown, so let's fake +-inf as min/max
+        return DFS(root, float("-INF"), float("INF"))
 
 
 # @lc code=end
-
