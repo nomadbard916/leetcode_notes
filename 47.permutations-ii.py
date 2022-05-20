ee865245b44410e3323679a129b688cc5747f7a9
nomadbard916 <neonnova916@gmail.com>
2022-05-20 16:51:15 +0800

    make 47 clearer

--- 47.permutations-ii.py
+++ 47.permutations-ii.py
@@ -10,32 +10,28 @@ from typing import List
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        perm_n = len(nums)
+        PERM_N = len(nums)
         ans = []
 
-        if perm_n == 0:
+        if PERM_N == 0:
             return ans
 
-        nums.sort()
-
-        def backtrack(current_path, option_list=nums):
-
-            if len(current_path) == perm_n and current_path not in ans:
+        def backtrack(current_path: list, option_list: list):
+            # ending condition
+            if len(current_path) == PERM_N and current_path not in ans:
                 ans.append(current_path)
                 return
 
             for i, num in enumerate(option_list):
-                # cut duplication here so there's no need to exclude already existed path in ans for later layer of backtrack
-                # if i > 0 and option_list[i] == option_list[i - 1]:
-                #     continue
-
                 updated_path = current_path + [num]
+                # cut duplication here,
+                # so there's no need to exclude already existed path in ans
+                # for later layer of backtrack
                 updated_options = option_list[:i] + option_list[i + 1 :]
 
                 backtrack(updated_path, updated_options)
 
-        current_path = []
-        backtrack(current_path)
+        backtrack([], nums)
 
         return ans
 
