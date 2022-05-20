7c82f360954eb5296d8386b13e65ebde828ef3da
nomadbard916 <neonnova916@gmail.com>
2022-05-20 20:34:30 +0800

    876

---
+++ 876.middle-of-the-linked-list.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode id=876 lang=python3
+#
+# [876] Middle of the Linked List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        turtle, hare = head, head
+        # when hare reaches tail with 2x speed,
+        # the travelling distance of turtle must be half of hare,
+        # i.e. the middle of linked list
+        while hare and hare.next:
+            turtle = turtle.next
+            hare = hare.next.next
+
+        return turtle
+
+
+# @lc code=end
