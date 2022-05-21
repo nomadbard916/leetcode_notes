133f0ad4f4c28773488b663b9bb3f92f1530a995
nomadbard916 <neonnova916@gmail.com>
2022-05-21 17:45:21 +0800

    clarify for 328

--- 328.odd-even-linked-list.py
+++ 328.odd-even-linked-list.py
@@ -6,47 +6,47 @@
 
 # @lc code=start
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
 class Solution:
     def oddEvenList(self, head: ListNode) -> ListNode:
         # refer to # 86 for two list chaining
 
-        # odd/even index follow the first one?
-        # !! not meaning odd/even value
         # space O(1) -> in place
         # time O(N) -> traversing all the nodes once
 
-        # sanity check for:
-        # no node in list
+        # sanity check for: no node in list => odd linked list must exist
         if not head:
             return head
 
         # initial head must be odd
-        current_odd = head
+        curr_odd = head
         # then next node of head must be even
-        current_even = current_odd.next
+        curr_even = curr_odd.next
 
         # head of even list must be the initial even node
-        even_head = current_even
+        even_head = curr_even
 
-        # as current_odd cannot be None for even list to attach onto, therefore even nodes are used for looping
-        while current_even and current_even.next:
-            current_odd.next = current_even.next
-            current_odd = current_odd.next
+        # as current_odd cannot be None for even list to be attached onto,
+        # even nodes are used for looping
+        while curr_even and curr_even.next:
+            curr_odd.next = curr_even.next
+            curr_odd = curr_odd.next
 
-            # as the first even node is already assigned, the traversal skips the first one, seeking the second next ones instead
+            # as the first even node is already assigned, traversal skips it,
+            # instead seeking the next ones , i.e. starting from current_even.next
             # the 'current_odd' here is actually already the next node
-            current_even.next = current_odd.next
-            current_even = current_even.next
+            curr_even.next = curr_odd.next
+            curr_even = curr_even.next
 
         # attach even linked list onto the last node of odd list
-        current_odd.next = even_head
+        curr_odd.next = even_head
 
         return head
 
 
 # @lc code=end
-
