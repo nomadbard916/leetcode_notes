b1c6ecd450ed89ebdbf840511ecbf72bb5a61fe8
nomadbard916 <neonnova916@gmail.com>
2022-05-22 23:49:38 +0800

    add sol2 to 287

--- 287.find-the-duplicate-number.py
+++ 287.find-the-duplicate-number.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import List
 
 #
 # @lc app=leetcode id=287 lang=python3
@@ -6,9 +7,11 @@ from collections import defaultdict
 # [287] Find the Duplicate Number
 #
 
+
 # @lc code=start
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
+        # sol1, O(n)
         seen = defaultdict(int)
 
         for num in nums:
@@ -17,6 +20,20 @@ class Solution:
             else:
                 return num
 
+        # sol2: abstract it to "linked list looping", O(n)
+        # see also 142, and here's guaranteed to be duplicated
+        # slow = nums[0]
+        # fast = nums[nums[0]]
+        # while slow != fast:
+        #     fast = nums[nums[fast]]
+        #     slow = nums[slow]
+        # fast = 0
+        # while slow != fast:
+        #     fast = nums[fast]
+        #     slow = nums[slow]
+        # return fast
+
+        # sol3: binary search
+
 
 # @lc code=end
-
