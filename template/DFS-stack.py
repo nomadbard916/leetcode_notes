936ecd513eb2ac53c847a8eb546f14fede6b2e5f
nomadbard916 <neonnova916@gmail.com>
2022-07-01 21:40:40 +0800

    add another implementation for DFS stack

--- template/DFS-stack.py
+++ template/DFS-stack.py
@@ -4,6 +4,9 @@ def dfs(graph, root):
 
     stack.append(root)
 
+    # actually we can just check against 'result'.
+    # making another 'visited' container just makes the concept clearer.
+    # see below for another implementation.
     visited = set()
     visited.add(root)
 
@@ -14,6 +17,7 @@ def dfs(graph, root):
         for neighbor in graph[currentVertex]:
             if neighbor in visited:
                 continue
+
             stack.append(neighbor)
             visited.add(neighbor)
     return result
@@ -31,3 +35,32 @@ graph = {
 
 print(dfs(graph, "A"))
 # ['A', 'E', 'C', 'F', 'D', 'B']
+
+
+# sol2
+def iterative_dfs(start_v):
+    discovered = []
+    stack = [start_v]
+    while stack:
+        v = stack.pop()
+
+        if v not in discovered:
+            discovered.append(v)
+            for w in graph[v]:
+                stack.append(w)
+
+    return discovered
+
+
+# adjacency list graph
+graph = {
+    1: [2, 3, 4],
+    2: [5],
+    3: [5],
+    4: [],
+    5: [6, 7],
+    6: [],
+    7: [3],
+}
+
+print(iterative_dfs(1))
