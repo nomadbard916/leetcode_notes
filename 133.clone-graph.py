b9c7914fc93aaee97e55aed65162fe6418ef22b6
nomadbard916 <neonnova916@gmail.com>
2022-07-03 22:36:56 +0800

    133

--- 133.clone-graph.py
+++ 133.clone-graph.py
@@ -5,16 +5,38 @@
 #
 
 # @lc code=start
-"""
+
+# c.f.: 138
+
 # Definition for a Node.
+from typing import Optional
+
+
 class Node:
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
-"""
+
 
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        
-# @lc code=end
+    def cloneGraph(self, node: Node) -> Node:
+        return self.dfs(node, {})
 
+    def dfs(self, node, visited) -> Optional[Node]:
+        if not node:
+            return
+        if node in visited:
+            return visited[node]
+
+        node_clone = Node(node.val, [])
+        visited[node] = node_clone
+
+        for neighbor in node.neighbors:
+            neighbor_copy = self.dfs(neighbor, visited)
+            if neighbor_copy:
+                node_clone.neighbors.append(neighbor_copy)
+
+        return node_clone
+
+
+# @lc code=end
