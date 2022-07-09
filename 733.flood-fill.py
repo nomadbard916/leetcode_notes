320b615daaa2e87695a817b30ec4254f13b67c44
nomadbard916 <neonnova916@gmail.com>
2022-07-09 20:55:38 +0800

    refine 733

--- 733.flood-fill.py
+++ 733.flood-fill.py
@@ -14,27 +14,30 @@ class Solution:
     ) -> List[List[int]]:
         # comare with 200
         LENGTH, WIDTH = len(image), len(image[0])
-        color = image[sr][sc]
 
-        if color == newColor:
+        curr_color = image[sr][sc]
+
+        if curr_color == newColor:
             return image
 
         def dfs(r, c):
-            if image[r][c] == color:
-                image[r][c] = newColor
+            if image[r][c] != curr_color:
+                return
 
-                upper_index = r - 1
-                lower_index1 = r + 1
-                left_index = c - 1
-                right_index = c + 1
-                if r >= 1:
-                    dfs(upper_index, c)
-                if r < LENGTH - 1:
-                    dfs(lower_index1, c)
-                if c >= 1:
-                    dfs(r, left_index)
-                if c < WIDTH - 1:
-                    dfs(r, right_index)
+            image[r][c] = newColor
+
+            upper_index = r - 1
+            lower_index = r + 1
+            left_index = c - 1
+            right_index = c + 1
+            if r >= 1:
+                dfs(upper_index, c)
+            if r < LENGTH - 1:
+                dfs(lower_index, c)
+            if c >= 1:
+                dfs(r, left_index)
+            if c < WIDTH - 1:
+                dfs(r, right_index)
 
         dfs(sr, sc)
 
