9d9381f601ed46bcda03d12ae0ca618dd55e2e24
nomadbard916 <neonnova916@gmail.com>
2025-03-25 17:33:32 +0800

    fix format

--- template/BFS-no_layer.py
+++ template/BFS-no_layer.py
@@ -1,18 +1,15 @@
 import collections
 
-
-class Solution:
-    def bfs(k):
-        # 使用双端队列，而不是数组。因为数组从头部删除元素的时间复杂度为 N，双端队列的底层实现其实是链表。
-        queue = collections.deque([root])
-        # 队列不空，生命不止！
-        while queue:
-            node = queue.popleft()
-            # 由于没有记录 steps，因此我们肯定是不需要根据层的信息去判断的。否则就用带层的模板了。
-            if (node ): #是我们要找到的
-                return node
-            if node.right:
-                queue.append(node.right)
-            if node.left:
-                queue.append(node.left)
-        return -1
+def bfs(root):
+    if root is None:
+        return
+    queue = collections.deque([root])
+    while queue:
+        node = queue.popleft()
+        if (node ): # is the node we're looking for
+            return node
+        if node.right:
+            queue.append(node.right)
+        if node.left:
+            queue.append(node.left)
+    return -1
