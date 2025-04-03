debc7c182308c2e718d3159e24f547bd1941b822
nomadbard916 <neonnova916@gmail.com>
2025-04-03 13:49:14 +0800

    sol2

--- 160.intersection-of-two-linked-lists.py
+++ 160.intersection-of-two-linked-lists.py
@@ -41,5 +41,15 @@ class Solution:
 
         return pre_node
 
+    # def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+    #     # p1 指向 A 链表头结点，p2 指向 B 链表头结点
+    #     p1, p2 = headA, headB
+    #     while p1 != p2:
+    #         # p1 走一步，如果走到 A 链表末尾，转到 B 链表
+    #         p1 = p1.next if p1 else headB
+    #         # p2 走一步，如果走到 B 链表末尾，转到 A 链表
+    #         p2 = p2.next if p2 else headA
+    #     return p1
+
 
 # @lc code=end
