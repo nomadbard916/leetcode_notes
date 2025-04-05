d81dc33d163e95aab0abfa0f2cb89c9822e5f7c1
nomadbard916 <neonnova916@gmail.com>
2025-04-05 19:35:35 +0800

    clearer commenting

--- 76.Minimum Window Substring.py
+++ 76.Minimum Window Substring.py
@@ -9,29 +9,31 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         char_cnt_needed = {}
+        char_cnt_window = {}
+
         # populate needed chars count mapping
         for c in t:
             char_cnt_needed[c] = char_cnt_needed.get(c, 0) + 1
 
-        left, right, valid_chars_cnt = 0, 0, 0
+        left, right = 0, 0
+        valid_chars_cnt = 0
 
-        # record the starting index and length of
-        # min covering substring
+        # record the starting index and length of min covering substring
         res_start = 0
         res_length = float("inf")
-        char_cnt_window = {}
         while right < len(s):
+            # * step 1: keep enlarging the window until required chars count met
             # char-moving into window
             c = s[right]
-            # enlarge the window
-            right += 1
             # update data within window
             if c in char_cnt_needed:
                 char_cnt_window[c] = char_cnt_window.get(c, 0) + 1
                 if char_cnt_window[c] == char_cnt_needed[c]:
                     valid_chars_cnt += 1
+            # enlarge the window
+            right += 1
 
-            # check if to shrink left window side
+            # * step 2: check if to shrink left window side
             while valid_chars_cnt == len(char_cnt_needed):
                 # update min covering substring
                 if right - left < res_length:
