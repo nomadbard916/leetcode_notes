afbd2633196afe1a45b1f58d459a6176d060671b
nomadbard916 <neonnova916@gmail.com>
2025-04-09 18:25:40 +0800

    init submission of 567

--- 567.Permutation in String.py
+++ 567.Permutation in String.py
@@ -6,11 +6,46 @@
 #
 
 # @lc code=start
+
+
+import collections
+
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
-# @lc code=end
+        need = collections.defaultdict(int)
+        window = collections.defaultdict(int)
+        for c in s1:
+            need[c] += 1
 
+        left, right, valid = 0, 0, 0
+
+        while right < len(s2):
+            c = s2[right]
+            right += 1
+            # data manipulation in window
+            if c in need:
+                window[c] += 1
+                if window[c] == need[c]:
+                    valid += 1
+
+            # check if to shrink left side of window
+            while right - left >= len(s1):
+                # determine if valid substring is found
+                if valid == len(need):
+                    return True
+                d = s2[left]
+                left += 1
+                # update data in window
+                if d in need:
+                    if window[d] == need[d]:
+                        valid -= 1
+                    window[d] -= 1
+        # substring not found
+        return False
+
+
+# @lc code=end
 
 
 #
@@ -23,4 +58,3 @@ class Solution:
 # @lcpr case=end
 
 #
-
