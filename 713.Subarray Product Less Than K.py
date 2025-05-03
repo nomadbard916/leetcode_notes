f87e0090472c73279fc1eb96d956cf893e765843
nomadbard916 <neonnova916@gmail.com>
2025-05-03 16:03:21 +0800

    init submit for 713

---
+++ 713.Subarray Product Less Than K.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=713 lang=python3
+# @lcpr version=30104
+#
+# [713] Subarray Product Less Than K
+#
+
+# @lc code=start
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        # sliding window, maybe compare with prefix sum?
+        l = 0
+        res = 0
+        product = 1
+
+        for r in range(len(nums)):
+            product *= nums[r]
+
+            if product >= k:
+                while product >= k and l <= r:
+                    product /= nums[l]
+                    l += 1
+
+            res += r - l + 1
+
+        return res
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [10,5,2,6]\n100\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3]\n0\n
+# @lcpr case=end
+
+#
