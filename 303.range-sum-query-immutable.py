6b84553af3d9b3e80cdd62e81a46a62351930af7
nomadbard916 <neonnova916@gmail.com>
2025-05-04 14:59:04 +0800

    correct formula for 303

--- 303.range-sum-query-immutable.py
+++ 303.range-sum-query-immutable.py
@@ -5,16 +5,23 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class NumArray:
+    prefix_sum: List[int]
+
     def __init__(self, nums: List[int]):
-        self.nums = nums
+        # construct prefix sum arr
+        self.prefix_sum = [0] * (len(nums) + 1)
+        for i in range(1, len(self.prefix_sum)):
+            self.prefix_sum[i] = self.prefix_sum[i - 1] + nums[i - 1]
 
     def sumRange(self, i: int, j: int) -> int:
-        return sum(self.nums[i : j + 1])
+        return self.prefix_sum[j + 1] - self.prefix_sum[i]
 
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(i,j)
 # @lc code=end
-
