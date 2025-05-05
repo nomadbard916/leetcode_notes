4cc468cdc756728baa4aea6b78ef2a6f2b3bb057
nomadbard916 <neonnova916@gmail.com>
2025-05-05 22:36:02 +0800

    cleaner sol1

--- 1314.Matrix Block Sum.py
+++ 1314.Matrix Block Sum.py
@@ -11,7 +11,41 @@ from typing import List
 
 class Solution:
     def matrixBlockSum(self, mat: List[List[int]], k: int) -> List[List[int]]:
-        # it's pretty much like 304 with padding prefix sum matrix
+        # sol 1: it's pretty much like 304 with padding prefix sum matrix
+        # Explanation on integral image:
+        # Here we use the technique of integral image, which is introduced to speed up block computation.
+        # Also, this technique is practical and common in the field of matrix operation and image processing such as filtering and feature extraction.
+        # Block sum formula on integral image. Block-sum of red rectangle = block-sum of D - block-sum of B - block-sum of C + block-sum of A
+        m = len(mat)
+        n = len(mat[0])
+
+        integral = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m):
+            for j in range(n):
+                integral[i + 1][j + 1] = (
+                    mat[i][j] + integral[i][j + 1] + integral[i + 1][j] - integral[i][j]
+                )
+
+        result = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+                r1 = max(0, i - k)
+                c1 = max(0, j - k)
+                r2 = min(m - 1, i + k)
+                c2 = min(n - 1, j + k)
+
+                result[i][j] = (
+                    integral[r2 + 1][c2 + 1]
+                    - integral[r2 + 1][c1]
+                    - integral[r1][c2 + 1]
+                    + integral[r1][c1]
+                )
+
+        return result
+
+        # sol2, but time complexity seems problematic and the structure is not clean enough
         m = len(mat)
         n = len(mat[0])
         answer = [[0] * n for _ in range(m)]
