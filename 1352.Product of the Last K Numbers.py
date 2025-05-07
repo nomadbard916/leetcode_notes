af0f8a0010c459ebe3cf97b3ea5a94db6729f025
nomadbard916 <neonnova916@gmail.com>
2025-05-07 19:51:32 +0800

    init 1352

---
+++ 1352.Product of the Last K Numbers.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=1352 lang=python3
+# @lcpr version=30104
+#
+# [1352] Product of the Last K Numbers
+#
+
+# @lc code=start
+class ProductOfNumbers:
+    def __init__(self):
+        self.prefix_prod_arr = [1]
+
+    def add(self, num: int) -> None:
+        if num == 0:
+            self.prefix_prod_arr = [1]
+        else:
+            self.prefix_prod_arr.append(self.prefix_prod_arr[-1] * num)
+
+    def getProduct(self, k: int) -> int:
+        if k >= len(self.prefix_prod_arr):
+            return 0
+
+        return self.prefix_prod_arr[-1] // self.prefix_prod_arr[-1 - k]
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
+# @lc code=end
