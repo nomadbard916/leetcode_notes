e5d356bd0e5ff527e2fc341ca8e1783dadc06d74
nomadbard916 <neonnova916@gmail.com>
2025-05-10 13:38:51 +0800

    add notes to sliding window

--- template/sliding-window.py
+++ template/sliding-window.py
@@ -3,6 +3,11 @@ def sliding_window(nums):
     right = 0
     window = []
 
+    # Doesn’t this sliding window framework also use a nested while loop? Why is the time complexity O(N)?
+
+    # Simply put, the pointers left and right never move backward (their values only increase), so each element in the string/array enters the window once and exits the window once. No element enters and exits the window multiple times, so the time complexity is proportional to the length of the string/array.
+
+    # In contrast, the brute-force solution with nested for loops has the j pointer move backward, which causes some elements to enter and exit the window multiple times, leading to a time complexity of 𝑂 ( 𝑁 squrare).
     while right < len(nums):
         curr_r_elm = nums[right]
         window.append(curr_r_elm)
