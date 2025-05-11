9a2cb42f5ae54ee0c7d2db3ce61ab90b8c16aa0f
nomadbard916 <neonnova916@gmail.com>
2025-05-11 20:03:31 +0800

    init 1008

---
+++ 1008.Construct Binary Search Tree from Preorder Traversal.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode id=1008 lang=python3
+# @lcpr version=30104
+#
+# [1008] Construct Binary Search Tree from Preorder Traversal
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List, Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
+        # Idea is simple.
+        # First item in preorder list is the root to be considered.
+        # For next item in preorder list, there are 2 cases to consider:
+        # If value is less than last item in stack, it is the left child of last item.
+        # If value is greater than last item in stack, pop it.
+        # The last popped item will be the parent and the item will be the right child of the parent.
+        root = TreeNode(preorder[0])
+        stack = [root]
+        for value in preorder[1:]:
+            if value < stack[-1].val:
+                stack[-1].left = TreeNode(value)
+                stack.append(stack[-1].left)
+            else:
+                while stack and stack[-1].val < value:
+                    last = stack.pop()
+                    last.right = TreeNode(value)
+                    stack.append(last.right)
+        return root
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [8,5,1,7,10,12]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,3]\n
+# @lcpr case=end
+
+#
