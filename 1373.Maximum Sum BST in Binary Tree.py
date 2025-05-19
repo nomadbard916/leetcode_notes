bea538109bac0e94798a6e4e139d2c1e2e9a29fc
nomadbard916 <neonnova916@gmail.com>
2025-05-19 19:08:15 +0800

    commenting for res

--- 1373.Maximum Sum BST in Binary Tree.py
+++ 1373.Maximum Sum BST in Binary Tree.py
@@ -23,12 +23,12 @@ class Solution:
 
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
         # on current standpoint, we need to know:
-        # 1. are left and right child tree BST
+        # 1. are left and right child tree BST?
         # 2. the max node of left child tree and the min node of right child tree
         # 3. the sum of left and right child tree nodes
         # but writing pre-order traversal needs many helper functions,
         # even worse, they are with recursion, making unnecessary complexity O(n^2)
-        # mentioning "child trees" => post order!
+        # ! mentioning "child trees" => post order!
 
         def find_vals_max_min_sum(root):
             # base case
@@ -36,27 +36,33 @@ class Solution:
                 return [1, float("inf"), float("-inf"), 0]
 
             # calculate left and right child tree recursively
-            left = find_vals_max_min_sum(root.left)
-            right = find_vals_max_min_sum(root.right)
+            left_child_res = find_vals_max_min_sum(root.left)
+            right_child_res = find_vals_max_min_sum(root.right)
 
             # * post order operations
             # search for return values by left and right
             # and update max_sum accordingly
+            # for fields in res arr:
+            # res[0] records whether the binary tree rooted at root is a BST; if it is 1, it means it is a BST, if 0, it means it is not a BST;
+            # res[1] records the minimum value among all nodes in the binary tree rooted at root;
+            # res[2] records the maximum value among all nodes in the binary tree rooted at root;
+            # res[3] records the sum of all node values in the binary tree rooted at root.
+
             res = [0, 0, 0, 0]
             if (
-                left[0] == 1
-                and right[0] == 1
-                and root.val > left[2]
-                and root.val < right[1]
+                left_child_res[0] == 1
+                and right_child_res[0] == 1
+                and root.val > left_child_res[2]
+                and root.val < right_child_res[1]
             ):
                 # the binary tree with "root" as root node is a BST
                 res[0] = 1
                 # for a leaf node, get the min value for itself to be the leaf
-                res[1] = min(left[1], root.val)
+                res[1] = min(left_child_res[1], root.val)
                 # for a leaf node, get the max value for itself to be the leaf
-                res[2] = max(right[2], root.val)
+                res[2] = max(right_child_res[2], root.val)
                 # calculate the sum of all nodes with "root" as the root node to this BST
-                res[3] = left[3] + right[3] + root.val
+                res[3] = left_child_res[3] + right_child_res[3] + root.val
                 # update the global variable
                 self.max_sum = max(self.max_sum, res[3])
 
