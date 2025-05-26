4fc2f37056df8cdb5f033daa28c49a79722256a0
nomadbard916 <neonnova916@gmail.com>
2025-05-26 23:38:03 +0800

    init 1926

---
+++ 1926.Nearest Exit from Entrance in Maze.py
@@ -0,0 +1,70 @@
+#
+# @lc app=leetcode id=1926 lang=python3
+# @lcpr version=30104
+#
+# [1926] Nearest Exit from Entrance in Maze
+#
+
+# @lc code=start
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def nearestExit(self, maze: List[List[str]], entrance: List[int]) -> int:
+        m = len(maze)
+        n = len(maze[0])
+        dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+        # * BFS framework with visited
+        q = deque()
+        q.append(entrance)
+        visited = [[False for _ in range(n)] for _ in range(m)]
+        visited[entrance[0]][entrance[1]] = True
+
+        steps = 0
+        while q:
+            q_size = len(q)
+            steps += 1
+            # try to visit all the nodes in q
+            for i in range(q_size):
+                cur_node = q.popleft()
+                # try to step out 1 step in dirs
+                for dir in dirs:
+                    x = cur_node[0] + dir[0]
+                    y = cur_node[1] + dir[1]
+                    # out of bound
+                    if (
+                        x < 0
+                        or x >= m
+                        or y < 0
+                        or y >= n
+                        or visited[x][y]
+                        or maze[x][y] == "+"
+                    ):
+                        continue
+                    # if it's exit
+                    if x == 0 or x == m - 1 or y == 0 or y == n - 1:
+                        return steps
+                    visited[x][y] = True
+                    q.append([x, y])
+        return -1
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [["+","+",".","+"],[".",".",".","+"],["+","+","+","."]]\n[1,2]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [["+","+","+"],[".",".","."],["+","+","+"]]\n[1,0]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [[".","+"]]\n[0,0]\n
+# @lcpr case=end
+
+#
