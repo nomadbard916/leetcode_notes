d44c209cd648625d2a90d4a7d8bf3d2bb772751a
nomadbard916 <neonnova916@gmail.com>
2025-05-28 21:53:35 +0800

    init 863

---
+++ 863.All Nodes Distance K in Binary Tree.py
@@ -0,0 +1,84 @@
+#
+# @lc app=leetcode id=863 lang=python3
+# @lcpr version=30201
+#
+# [863] All Nodes Distance K in Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from collections import deque
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        # ! sol1: BFS with parent mapping (recommended)
+        if k == 0:
+            return [target.val]
+
+        # build parent mapping to create undirected graph representation
+        parent_map = {}
+
+        def build_parent_map(node, parent):
+            if not node:
+                return
+            parent_map[node] = parent
+            build_parent_map(node.left, node)
+            build_parent_map(node.right, node)
+
+        build_parent_map(root, None)
+
+        # BFS from target node
+        q = deque([target])
+        visited_set = {target}
+        distance = 0
+
+        while q and distance < k:
+            q_size = len(q)
+            distance += 1
+            for _ in range(q_size):
+                curr_node = q.popleft()
+                # check all neighbors: left child, right child, parent
+                neighbors = []
+                if curr_node.left:
+                    neighbors.append(curr_node.left)
+                if curr_node.right:
+                    neighbors.append(curr_node.right)
+                if parent_map[curr_node]:
+                    neighbors.append(parent_map[curr_node])
+
+                for n in neighbors:
+                    if n not in visited_set:
+                        visited_set.add(n)
+                        q.append(n)
+
+        # collect all nodes at distance k
+        result = []
+        while q:
+            curr_val = q.popleft().val
+            result.append(curr_val)
+
+        return result
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [3,5,1,6,2,0,8,null,null,7,4]\n5\n2\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n1\n3\n
+# @lcpr case=end
+
+#
