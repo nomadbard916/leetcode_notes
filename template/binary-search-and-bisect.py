9051cf93e94c9f8cdf8ee23a819d513809290184
nomadbard916 <neonnova916@gmail.com>
2025-06-08 13:01:49 +0800

    add notes

--- template/binary-search-and-bisect.py
+++ template/binary-search-and-bisect.py
@@ -53,7 +53,11 @@ def left_bound(nums: List[int], target: int) -> int:
             right = mid
 
     return left
-    # 如果 target 不存在，搜索左侧边界的二分搜索返回的索引是大于 target 的最小索引。
+    # 当目标元素 target 不存在数组 nums 中时，搜索左侧边界的二分搜索的返回值可以做以下几种解读：
+    # 1、返回的这个值是 nums 中大于等于 target 的最小元素索引。
+    # 2、返回的这个值是 target 应该插入在 nums 中的索引位置。
+    # 3、返回的这个值是 nums 中小于 target 的元素个数。
+    # 比如在有序数组 nums = [2,3,5,7] 中搜索 target = 4，搜索左边界的二分算法会返回 2，你带入上面的说法，都是对的。
 
     # 如果想让 target 不存在时返回 -1 其实很简单，
     # 在返回的时候额外判断一下 nums[left] 是否等于 target 就行了，如果不等于，就说明 target 不存在。
