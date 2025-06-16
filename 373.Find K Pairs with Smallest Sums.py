fd8834be69b6ea9f415e8075ccc90e375c0cf303
nomadbard916 <neonnova916@gmail.com>
2025-06-16 08:59:51 +0800

    add notes

--- 373.Find K Pairs with Smallest Sums.py
+++ 373.Find K Pairs with Smallest Sums.py
@@ -14,38 +14,39 @@ class Solution:
     def kSmallestPairs(
         self, nums1: List[int], nums2: List[int], k: int
     ) -> List[List[int]]:
-        # it's essentially the extension of # 23 "merge k sorted lists"
+        # * it's essentially the extension of # 23 "merge k sorted lists"
         # priority queue is used here as there are some convenient functionalities like empty()
         pq = PriorityQueue()
 
         # init pq
-        nums2_first_idx = 0
+        res_first_idx = 0
         for i in range(len(nums1)):
             # just put in some smaller ones,
             # as nums1[i] + nums2[0] must be the smallest from nums1 side
             pq.put(
                 (
-                    nums1[i] + nums2[nums2_first_idx],
+                    nums1[i] + nums2[res_first_idx],
                     nums1[i],
-                    nums2[nums2_first_idx],
-                    nums2_first_idx,
+                    nums2[res_first_idx],
+                    res_first_idx,
                 )
             )
 
         res = []
 
-        # merge multiple sorted lists
+        # * merge multiple sorted lists
         while not pq.empty() and k > 0:
-            _, num1, num2, idx = pq.get()
+            _, num1, num2, curr_idx = pq.get()
             k -= 1
-            # add the next node to pq
-            next_idx = idx + 1
+            # put smaller nodes into pq from nums2 side
+            next_idx = curr_idx + 1
             if next_idx < len(nums2):
                 pq.put((num1 + nums2[next_idx], num1, nums2[next_idx], next_idx))
 
-            # sort asc by pair elements
+            # just append pairs so their some is guaranteed to be ascending from min
             pair = [num1, num2]
             res.append(pair)
+
         return res
 
 
