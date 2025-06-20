a8706dd20412b1b9abda2a3a768b231127ee2545
nomadbard916 <neonnova916@gmail.com>
2025-06-20 09:35:19 +0800

    add notes

--- 1593.Split a String Into the Max Number of Unique Substrings.py
+++ 1593.Split a String Into the Max Number of Unique Substrings.py
@@ -10,19 +10,24 @@ class Solution:
     def maxUniqueSplit(self, s: str) -> int:
         def backtrack(start: int, used: set[str]) -> int:
             max_splits = 0
+            # * ending condition: reaching the end of the string
             if start == len(s):
                 return max_splits
 
+            # try all possible substrings starting from start
             for end in range(start + 1, len(s) + 1):
                 substring = s[start:end]
 
                 if substring not in used:
                     used.add(substring)
 
+                    # * make the decision with option list and path bringing down to next recursion level
+                    # current level + result from recursive remaining
                     current_splits = 1 + backtrack(end, used)
 
                     max_splits = max(max_splits, current_splits)
 
+                    # * cancel after the decision is made
                     used.remove(substring)
 
             return max_splits
