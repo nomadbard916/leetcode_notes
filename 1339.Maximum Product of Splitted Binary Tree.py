ff863c333f232a1c78180ea8fbbd00c158cb91d0
nomadbard916 <neonnova916@gmail.com>
2025-07-03 22:17:20 +0800

    init 1339

---
+++ 1339.Maximum Product of Splitted Binary Tree.py
@@ -0,0 +1,58 @@
+#
+# @lc app=leetcode id=1339 lang=python3
+# @lcpr version=30201
+#
+# [1339] Maximum Product of Splitted Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def maxProduct(self, root: Optional[TreeNode]) -> int:
+        MOD = 10**9 + 7
+        subtree_sums = []
+
+        def dfs(node: Optional[TreeNode]) -> int:
+            if not node:
+                return 0
+
+            # calculate sum of current subtree
+            current_sum = node.val + dfs(node.left) + dfs(node.right)
+            subtree_sums.append(current_sum)
+            return current_sum
+
+        total_sum = dfs(root)
+        max_product = 0
+
+        # check all possible splits (except the root itself)
+        for subtree_sum in subtree_sums[:-1]:  # exclude root
+            remaining_sum = total_sum - subtree_sum
+            product = subtree_sum * remaining_sum
+            max_product = max(max_product, product)
+
+        return max_product % MOD
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [1,2,3,4,5,6]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,null,2,3,4,null,null,5,6]\n
+# @lcpr case=end
+
+#
