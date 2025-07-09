cfeb330b14c97ced27653a011b01804eccf047e9
nomadbard916 <neonnova916@gmail.com>
2025-07-09 20:37:58 +0800

    init 1448

---
+++ 1448.Count Good Nodes in Binary Tree.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=1448 lang=python3
+# @lcpr version=30201
+#
+# [1448] Count Good Nodes in Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        def dfs(node: Optional[TreeNode], max_val: int) -> int:
+            if not node:
+                return 0
+
+            good_count = 0
+            if node.val >= max_val:
+                good_count = 1
+
+            new_max = max(max_val, node.val)
+
+            good_count += dfs(node.left, new_max)
+            good_count += dfs(node.right, new_max)
+
+            return good_count
+
+        return dfs(root, root.val)
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [3,1,4,3,null,1,5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [3,3,null,4,2]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1]\n
+# @lcpr case=end
+
+#
