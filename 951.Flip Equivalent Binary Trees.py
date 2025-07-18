f1d7374582c7846eb1715094d0c7bafdc0168b58
nomadbard916 <neonnova916@gmail.com>
2025-07-18 20:53:03 +0800

    add notes

--- 951.Flip Equivalent Binary Trees.py
+++ 951.Flip Equivalent Binary Trees.py
@@ -22,20 +22,33 @@ class Solution:
         # we can define a flip operation as follows: choose any node, and swap the left and right child subtrees.
         # A binary tree X is flip equivalent to a binary tree Y if and only if
         # we can make X equal to Y after some number of flip operations.
+
+        # Base case: both nodes are None
         if not root1 and not root2:
             return True
+        # Base case: one node is None, the other is not
         if not root1 or not root2:
             return False
+        # Base case: values are different
         if root1.val != root2.val:
             return False
 
+        # we don't include below condition
+        # if root1.val == root2.val:
+        #     return True
+        # Just because two nodes have the same value doesn't mean the entire subtrees rooted at those nodes are flip equivalent!
+
+        # Recursive case: check both possibilities
+        # Option 1: No flip needed (left matches left, right matches right)
         no_flip = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(
             root1.right, root2.right
         )
+        # Option 2: Flip needed (left matches right, right matches left)
         flip = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(
             root1.right, root2.left
         )
 
+        # Trees are flip equivalent if either option works
         return no_flip or flip
 
         # Time and Space Complexity
