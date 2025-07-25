0a16452b8e1522649448d2d2c298a2c2d7fa312b
nomadbard916 <neonnova916@gmail.com>
2025-07-25 23:35:23 +0800

    init 416

---
+++ 416.Partition Equal Subset Sum.py
@@ -0,0 +1,43 @@
+#
+# @lc app=leetcode id=416 lang=python3
+# @lcpr version=30201
+#
+# [416] Partition Equal Subset Sum
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        total_sum = sum(nums)
+
+        if total_sum % 2 != 0:
+            return False
+
+        target = total_sum // 2
+
+        dp = [False] * (target + 1)
+        dp[0] = True
+
+        for num in nums:
+            for j in range(target, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+
+        return dp[target]
+
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# [1,5,11,5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [1,2,3,5]\n
+# @lcpr case=end
+
+#
