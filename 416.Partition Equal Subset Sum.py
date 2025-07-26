94d705db5d9a57f8e0a8cdf99bd5d4b15731cc5b
nomadbard916 <neonnova916@gmail.com>
2025-07-26 20:55:16 +0800

    add notes

--- 416.Partition Equal Subset Sum.py
+++ 416.Partition Equal Subset Sum.py
@@ -25,6 +25,9 @@ class Solution:
         dp_target_num = [False] * (target + 1)  # target itself and dummy start 0
         dp_target_num[0] = True  # Sum of 0 is always achievable (empty subset)
 
+        # * We Process One Number at a Time, But the DP Table Accumulates ALL Combinations
+        # dp[j] represents whether sum j can be made using ANY combination of numbers processed so far,
+        # not just the current number.
         for num in nums:
             # * Iterate backwards to avoid using the same number twice
             # We don't have to start from the last one! We can start from anywhere and go backwards, but we must go backwards to avoid dependency cycles.
@@ -48,7 +51,14 @@ class Solution:
             # The "last to first" is just the most convenient way to ensure proper dependency ordering.
             for j in range(target, num - 1, -1):
                 # Only update if we can make sum (j - num)
-                # don't worry if the previous one as not set yet, we have idx 0 True
+                # don't worry if the previous one has not set yet,
+                # The Key Insight: dp[j-num] was determined in PREVIOUS iterations, not the current one!
+                # * This single line is asking:
+                # "For every sum we could make before, can we make a new sum by adding the current number?"
+                # Why This Works:
+                # - dp[j-num] = True means "some combination of previous numbers sums to j-num"
+                # - We don't need to know WHICH combination - just that it exists
+                # - Adding current number num gives us sum j
                 if dp_target_num[j - num]:
                     dp_target_num[j] = True
 
