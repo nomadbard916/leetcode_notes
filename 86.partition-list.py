c181b1d57f250b00f3c2c3a2e03267d7f93652cf
nomadbard916 <neonnova916@gmail.com>
2025-07-31 21:28:19 +0800

    renaming

--- 86.partition-list.py
+++ 86.partition-list.py
@@ -17,27 +17,27 @@ class Solution:
         # refer to # 328 for odd even list chaining
 
         # * use dummy nodes as outsider starting point
-        before_node = dummy_before = ListNode()
-        after_node = dummy_after = ListNode()
+        lt_head = dummy_lt = ListNode()
+        gte_head = dummy_gte = ListNode()
 
         # * traverse the linked list
         # find the heads of two lists and link them in the process
         while head:
             if head.val < x:
-                before_node.next = head
-                before_node = before_node.next
+                lt_head.next = head
+                lt_head = lt_head.next
             # this list will contain the target
             else:
-                after_node.next = head
-                after_node = after_node.next
+                gte_head.next = head
+                gte_head = gte_head.next
 
             head = head.next
 
         # * Finish by connecting the two lists and terminating the after list.
-        after_node.next = None
-        before_node.next = dummy_after.next
+        gte_head.next = None
+        lt_head.next = dummy_gte.next
 
-        return dummy_before.next
+        return dummy_lt.next
 
 
 # @lc code=end
