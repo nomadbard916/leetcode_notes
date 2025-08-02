33f75095d07ad53fa0da26ed2e657b80de987747
nomadbard916 <neonnova916@gmail.com>
2025-08-02 10:51:41 +0800

    another sol by reversing and comparing

--- 234.palindrome-linked-list.py
+++ 234.palindrome-linked-list.py
@@ -1,4 +1,5 @@
 from collections import deque
+from typing import Optional
 
 #
 # @lc app=leetcode id=234 lang=python3
@@ -17,6 +18,42 @@ class ListNode:
 
 class Solution:
     def isPalindrome(self, head: ListNode) -> bool:
+        # ! sol1: optimal and complicated
+        # 1. find the middle
+        # 2. reverse second half
+        # 3. compare
+        def reverse_linked_list(head: Optional[ListNode]) -> Optional[ListNode]:
+            prev = None
+            curr = head
+
+            while curr:
+                next_tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = next_tmp
+
+            return prev
+
+        if not head or not head.next:
+            return True
+
+        slow: Optional[ListNode] = head
+        fast: Optional[ListNode] = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        second_half = reverse_linked_list(slow)
+        first_half = head
+
+        while second_half:
+            if first_half.val != second_half.val:
+                return False
+            first_half = first_half.next
+            second_half = second_half.next
+
+        return True
+
         # ! sol2: convert linked list to array, then check with two pointers
         # easier to understand, but uses unnecessary extra space
         values = []
