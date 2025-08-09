39d95796e2ee2004eef53224fb49816c5a53c544
nomadbard916 <neonnova916@gmail.com>
2025-08-09 13:17:26 +0800

    commenting

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -16,15 +16,24 @@ class Solution:
         if min(coins) > amount:
             return -1
 
-        DP = [amount + 1] * (amount + 1)
+        # just make it a little bigger to be impossible, instead of INF
+        IMPOSSIBLE_VALUE = amount + 1
+
+        # dp[i] represents the minimum coins needed to make amount i
+        # Initialize with amount + 1 (impossible value) except dp[0] = 0
+        DP = [IMPOSSIBLE_VALUE] * (amount + 1)
         DP[0] = 0
 
         for curr_amount in range(1, amount + 1):
             for coin in coins:
+                # If this coin can be used (doesn't exceed current amount)
                 if coin <= curr_amount:
+                    # Update dp[current_amount] with minimum of:
+                    # 1. Current value
+                    # 2. Using this coin + minimum coins for remaining amount
                     DP[curr_amount] = min((DP[curr_amount - coin] + 1), DP[curr_amount])
 
-        if DP[amount] == amount + 1:
+        if DP[amount] == IMPOSSIBLE_VALUE:
             return -1
 
         return DP[amount]
