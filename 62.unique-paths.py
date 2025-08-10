01d5cd6e30b305a543131e7a447f1f99908f463e
nomadbard916 <neonnova916@gmail.com>
2025-08-10 12:24:20 +0800

    fix definition of m,n,x,y

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -7,23 +7,21 @@
 # @lc code=start
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # tabulation; mn is wrong here, see the description (7*3 mistaken to be n*m)
-        table = [[0 for x in range(m)] for y in range(n)]
+        dp = [[0] * n for _ in range(m)]
 
         # every other grid should be the sum of (x-1, y), (x, y-1) following the restricted moving direction
-        for x in range(m):
-            for y in range(n):
+        for x in range(n):
+            for y in range(m):
                 # first row and column should all be 1
                 if x == 0:
-                    table[y][x] = 1
+                    dp[y][x] = 1
                 elif y == 0:
-                    table[y][x] = 1
+                    dp[y][x] = 1
                 # every other grid should be the sum of (x-1, y), (x, y-1) following the restricted moving direction
                 else:
-                    table[y][x] = table[y - 1][x] + table[y][x - 1]
+                    dp[y][x] = dp[y - 1][x] + dp[y][x - 1]
 
-        return table[-1][-1]
+        return dp[m - 1][n - 1]
 
 
 # @lc code=end
-
