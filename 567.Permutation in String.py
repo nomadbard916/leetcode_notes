f5eb915ef0e7654f0f2522f303a0bc9bf39277e4
nomadbard916 <neonnova916@gmail.com>
2025-08-11 23:05:18 +0800

    add comments

--- 567.Permutation in String.py
+++ 567.Permutation in String.py
@@ -8,17 +8,17 @@
 # @lc code=start
 
 
-import collections
+from collections import defaultdict
 
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # * sliding window => two pointer
-        need = collections.defaultdict(int)
-        window = collections.defaultdict(int)
+        needed = defaultdict(int)
+        window = defaultdict(int)
         # fill in container first
         for c in s1:
-            need[c] += 1
+            needed[c] += 1
 
         l, r = 0, 0
         valid_chars_cnt = 0
@@ -27,23 +27,27 @@ class Solution:
             # * step 1: keep enlarging the window until required chars count met
             curr_c_r = s2[r]
             # data manipulation in window
-            if curr_c_r in need:
+            if curr_c_r in needed:
                 window[curr_c_r] += 1
-                if window[curr_c_r] == need[curr_c_r]:
+                if window[curr_c_r] == needed[curr_c_r]:
                     valid_chars_cnt += 1
             r += 1
 
             # * step 2: check if to shrink left window side
-            # the window is actually a fixed length one,
-            # so we may change the "while" structure to "if"
-            while r - l >= len(s1):
+            # the window length is actually fixed to s1,
+            # so we should use "if" instead of "while"
+            # In this problem, you are looking for a substring in s2 that is a permutation of s1.
+            # Since a permutation must have the same length as s1,
+            # the sliding window should always be of length len(s1) as it moves through s2.
+            # That's why the code checks if r - l >= len(s1):—to ensure the window size matches s1's length before checking for a valid permutation.
+            if r - l >= len(s1):
                 # determine if valid substring is found
-                if valid_chars_cnt == len(need):
+                if valid_chars_cnt == len(needed):
                     return True
                 curr_c_l = s2[l]
                 # update data in window
-                if curr_c_l in need:
-                    if window[curr_c_l] == need[curr_c_l]:
+                if curr_c_l in needed:
+                    if window[curr_c_l] == needed[curr_c_l]:
                         valid_chars_cnt -= 1
                     window[curr_c_l] -= 1
                 l += 1
