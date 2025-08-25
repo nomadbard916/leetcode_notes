d0a00d87cd1d0abc5bf38f1c54f9a859b66109ae
nomadbard916 <neonnova916@gmail.com>
2025-08-25 09:37:10 +0800

    init 721

--- 721.Accounts Merge.py
+++ 721.Accounts Merge.py
@@ -6,16 +6,75 @@
 #
 
 # @lc code=start
+from collections import defaultdict
+from typing import List
+
+
+class UnionFind:
+    def __init__(self, n) -> None:
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, x):
+        if self.parent[x] != x:
+            # Path compression
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+
+        if px == py:
+            return
+
+        # union by rank
+        if self.rank[px] < self.rank[py]:
+            px, py = py, px
+
+        self.parent[py] = px
+
+        if self.rank[px] == self.rank[py]:
+            self.rank[px] += 1
+
+
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         # ! sol1: union find, which is the common solution
+        n = len(accounts)
+        uf = UnionFind(n)
+
+        email_to_account = {}
+
+        for i, account in enumerate(accounts):
+            for email in account[1:]:
+                if email in email_to_account:
+                    uf.union(i, email_to_account[email])
+                else:
+                    email_to_account[email] = i
+
+        groups = defaultdict(list)
+        for i in range(n):
+            root = uf.find(i)
+            groups[root].append(i)
+
+        result = []
+        for account_indices in groups.values():
+            emails = set()
+            name = ""
+            for idx in account_indices:
+                if not name:
+                    name = accounts[idx][0]
+                emails.update(accounts[idx][1:])
+
+            merged_account = [name] + sorted(emails)
+            result.append(merged_account)
+
+        return result
 
         # ! sol2: graph DFS
 
 
-
 # @lc code=end
-
 
 
 #
@@ -28,4 +87,3 @@ class Solution:
 # @lcpr case=end
 
 #
-
