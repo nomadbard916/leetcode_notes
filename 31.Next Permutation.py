6be640d70a1960686df22f74c11f30241df233d5
nomadbard916 <neonnova916@gmail.com>
2025-12-05 10:01:49 +0800

    init submit 31

--- 31.Next Permutation.py
+++ 31.Next Permutation.py
@@ -62,6 +62,52 @@ class Solution:
 
         # tricky keywords
 
+        """
+        Modifies nums in-place to be the next lexicographical permutation.
+
+        Algorithm:
+        1. Find the rightmost 'pivot' where nums[i] < nums[i+1]
+        2. If no pivot exists, reverse entire array (it's the largest permutation)
+        3. Find the smallest number in nums[i+1:] that's greater than nums[i]
+        4. Swap nums[i] with that number
+        5. Reverse nums[i+1:] to get the smallest arrangement
+        """
+        n = len(nums)
+
+        # step 1: find the pivot point (rightmost i where nums[i] < nums[i+1])
+        # we scan from right to left to find where the sequence stops being descdending
+        pivot = -1
+        for i in range(n - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                pivot = i
+                break
+
+        # step 2: if no pivot found, array is in descending order
+        # this is the largest permutation, so wrap around the smallest
+        if pivot == -1:
+            nums.reverse()
+            return
+
+        # step 3: find the smallest number > nums[pivot] in the suffix
+        # since nums[pivot+1:] is in descending order, we scan from right
+        successor = -1
+        for i in range(n - 1, pivot, -1):
+            if nums[i] > nums[pivot]:
+                successor = i
+                break
+
+        # step 4: swap pivot with its successor
+        nums[pivot], nums[successor] = nums[successor], nums[pivot]
+
+        # step 5: reverse the suffix to get the smallest arrangement
+        # after swapping, nums[pivot+1:] is still in descending order
+        # we need ascending order for the next smallest permutation
+        l, r = pivot + 1, n - 1
+        while l < r:
+            nums[l], nums[r] = nums[r], nums[l]
+            l += 1
+            r -= 1
+
 
 # @lc code=end
 
