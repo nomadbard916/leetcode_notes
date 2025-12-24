d80b527fb80cde433398bdf2162ee2ba77a26711
nomadbard916 <neonnova916@gmail.com>
2025-12-24 10:01:31 +0800

    init submit of 25

--- 25.Reverse Nodes in k-Group.py
+++ 25.Reverse Nodes in k-Group.py
@@ -64,6 +64,43 @@ class Solution:
         6. repeat until insufficient nodes remain
         """
 
+        def get_kth_node(curr: Optional[ListNode], k: int) -> Optional[ListNode]:
+            while curr and k > 0:
+                curr = curr.next
+                k -= 1
+            return curr
+
+        if not head or k == 1:
+            return head
+
+        dummy = ListNode(0)
+        dummy.next = head
+
+        prev_group_end = dummy
+
+        while True:
+            kth_node = get_kth_node(prev_group_end, k)
+
+            if not kth_node:
+                break
+
+            next_group_start = kth_node.next
+
+            prev, curr = kth_node.next, prev_group_end.next
+
+            while curr != next_group_start:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+
+            original_group_start = prev_group_end.next
+            prev_group_end.next = kth_node
+
+            prev_group_end = original_group_start
+
+        return dummy.next
+
 
 # @lc code=end
 
