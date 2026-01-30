cb6d0771aefa2a82af463cacaf974add9a5ad040
nomadbard916 <neonnova916@gmail.com>
2026-01-30 22:56:13 +0800

    init wrong submission

--- 735.Asteroid Collision.py
+++ 735.Asteroid Collision.py
@@ -30,6 +30,34 @@ class Solution:
         * pattern kws:
         ?
         """
+        tmp_stack = []
+        # no need to sanity check
+
+        for i, a in enumerate(asteroids):
+            if i == 0:
+                tmp_stack.append(a)
+                continue
+
+            # start here
+            prev_a = asteroids[i-1]
+            if a * prev_a < 0:
+                # start comparison
+                # a smaller
+                if a < prev_a:
+                    # destroyed
+                    continue
+                # a bigger
+                if a > prev_a:
+                    tmp_stack.pop()
+                    tmp_stack.append
+                # equal
+                elif a == prev_a:
+                    tmp_stack.pop()
+            else:
+                tmp_stack.append(a)
+
+        return tmp_stack
+
 
 # @lc code=end
 
