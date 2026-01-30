02999b4492c23322686c59973bedac87a516c117
nomadbard916 <neonnova916@gmail.com>
2026-01-30 10:17:07 +0800

    init 735

---
+++ 735.Asteroid Collision.py
@@ -0,0 +1,56 @@
+#
+# @lc app=leetcode id=735 lang=python3
+# @lcpr version=30305
+#
+# [735] Asteroid Collision
+#
+
+# @lc code=start
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        """
+        * nouns and verbs:
+        intergers in a row, indices... position, array, absolute value: size, sign: direction, same speed, state, explode: smaller one, both when same size , never meet: same direction
+
+        * pattern kws
+        in a row
+
+        * structural kws
+        no? asteroids[i] != 0
+
+        * algo concepts:
+        in a row => stack
+
+        * mental categories:
+        ?
+
+        * tricky kws:
+        explosion rules
+
+        * pattern kws:
+        ?
+        """
+
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# [5,10,-5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [8,-8]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [10,2,-5]\n
+# @lcpr case=end
+
+# @lcpr case=start
+# [3,5,-6,2,-1,4]\n
+# @lcpr case=end
+
+#
+
