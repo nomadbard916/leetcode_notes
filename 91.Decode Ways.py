700968e57204d62bfdf211117998169c3ac5edbd
nomadbard916 <neonnova916@gmail.com>
2026-05-16 10:37:57 +0800

    init kws

---
+++ 91.Decode Ways.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode id=91 lang=python3
+# @lcpr version=30403
+#
+# [91] Decode Ways
+#
+
+# @lc code=start
+from __future__ import annotations
+
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        """
+        * noun and verb
+        - string of numbers
+        - decoded
+        - rules:
+        prefix 0 not allowed
+        1~26
+        - different ways
+        - grouping
+        * pattern kws
+        - backtracking?
+        - two pointer?
+        - recursion?
+        - dp?
+
+        * constraint kws
+        answer in 32-bit integer
+        * map to algo
+        * mental model
+        * tricky
+        * problem specific pattern
+        """
+        ans = 0
+        return ans
+
+# @lc code=end
+
+
+
+#
+# @lcpr case=start
+# "12"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "226"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "06"\n
+# @lcpr case=end
+
+#
+
