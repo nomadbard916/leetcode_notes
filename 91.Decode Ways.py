5c42a9cb64b857de657383e01a747346bf90388d
nomadbard916 <neonnova916@gmail.com>
2026-05-17 08:57:49 +0800

    first try with fail

--- 91.Decode Ways.py
+++ 91.Decode Ways.py
@@ -6,7 +6,6 @@
 #
 
 # @lc code=start
-from __future__ import annotations
 
 
 class Solution:
@@ -22,7 +21,7 @@ class Solution:
         - grouping
         * pattern kws
         - backtracking?
-        - two pointer?
+        - two pointer? ...no
         - recursion?
         - dp?
 
@@ -31,13 +30,43 @@ class Solution:
         * map to algo
         * mental model
         * tricky
+        - each code cannot start from 0
+        - code range can only be 1~26
         * problem specific pattern
         """
-        ans = 0
-        return ans
+        n = len(s)
+        dp = [0] * n
+
+        for i, digit_char in enumerate(s):
+            if i == 0:
+                if digit_char == 0:
+                    dp[i] = 0
+                else:
+                    dp[i] = 1
+                continue
+            if i == 1:
+                if digit_char == 0:
+                    dp[i] = dp[i - 1]
+                else:
+                    dp[i] = dp[i] + dp[i - 1]
+                continue
+
+            if digit_char == "0":
+                dp[i] = dp[i - 1]
+                continue
+
+            # consider only 1 digit
+            if s[i - 1] != "0":
+                dp[i] += dp[i - 1]
+            # consider two digits
+            two_digits = int(s[i - 2 : i])
+            if 10 <= two_digits <= 26:
+                dp[i] += dp[i - 2]
+
+        return dp[-1]
+
 
 # @lc code=end
-
 
 
 #
@@ -54,4 +83,3 @@ class Solution:
 # @lcpr case=end
 
 #
-
